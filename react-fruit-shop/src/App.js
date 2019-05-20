112f01f3e5fa46f332b894edee83ad0532f20f5c
Igor Radomirov <igor.radomirov@gmail.com>
2019-05-20 23:36:04 +0200

    App has some class now :)

--- react-fruit-shop/src/App.js
+++ react-fruit-shop/src/App.js
@@ -1,45 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        React Fruit Shop
-      </header>
-
-      <div className="row-grid">
-          <div className="col-item md-cover-2 lg-cover-4">
-            responsive length
-          </div>
-        </div>
+class App extends Component {
+  
+  // render method for this component
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          React Fruit Shop
+        </header>
 
         <div className="row-grid">
-          <div className="col-item sm-cover-1">
-            xs-1
+            <div className="col-item md-cover-2 lg-cover-4">
+              responsive length
+            </div>
           </div>
-          <div className="col-item sm-cover-2">
-            xs-2
-          </div>
-          <div className="col-item sm-cover-3">
-            xs-3
-          </div>
-          <div className="col-item sm-cover-6">
-            
-            <div className="row-grid">
-              <div className="col-item md-cover-4 lg-cover-6">
-                A
-              </div>
-              <div className="col-item md-cover-8 lg-cover-6">
-                B
+
+          <div className="row-grid">
+            <div className="col-item sm-cover-1">
+              xs-1
+            </div>
+            <div className="col-item sm-cover-2">
+              xs-2
+            </div>
+            <div className="col-item sm-cover-3">
+              xs-3
+            </div>
+            <div className="col-item sm-cover-6">
+              
+              <div className="row-grid">
+                <div className="col-item md-cover-4 lg-cover-6">
+                  A
+                </div>
+                <div className="col-item md-cover-8 lg-cover-6">
+                  B
+                </div>
               </div>
             </div>
           </div>
-        </div>
 
-    </div>
-  );
+      </div>
+    );
+  }
 }
 
 export default App;
