f98e2f631b791e6c6aa1423580ae91e0581a9b64
Igor Radomirov <igor.radomirov@gmail.com>
2019-05-20 23:28:54 +0200

    Basic responsive behaviour added.

--- react-fruit-shop/src/App.js
+++ react-fruit-shop/src/App.js
@@ -1,24 +1,43 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        React Fruit Shop
       </header>
+
+      <div className="row-grid">
+          <div className="col-item md-cover-2 lg-cover-4">
+            responsive length
+          </div>
+        </div>
+
+        <div className="row-grid">
+          <div className="col-item sm-cover-1">
+            xs-1
+          </div>
+          <div className="col-item sm-cover-2">
+            xs-2
+          </div>
+          <div className="col-item sm-cover-3">
+            xs-3
+          </div>
+          <div className="col-item sm-cover-6">
+            
+            <div className="row-grid">
+              <div className="col-item md-cover-4 lg-cover-6">
+                A
+              </div>
+              <div className="col-item md-cover-8 lg-cover-6">
+                B
+              </div>
+            </div>
+          </div>
+        </div>
+
     </div>
   );
 }
